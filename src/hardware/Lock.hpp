1e89a1594a46e0145dde404c0b633ad9406dbec4
Zhangzqs <2428698039@qq.com>
2021-01-18 14:08:49 +0800

    新增舵机库和添加锁的硬件抽象类

---
+++ src/hardware/Lock.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <Arduino.h>
+#include <Servo.h>
+
+class Lock
+{
+private:
+    uint8_t pin;
+    int startDegree; //起始角度
+    int endDegree;   //终止角度
+    Servo servo;
+
+public:
+    explicit Lock(uint8_t pin, int startDegree = 0, int endDegree = 20) //
+        : pin(pin), startDegree(startDegree), endDegree(endDegree)      //初始化各个参数
+    {
+        servo.attach(pin); //分配舵机资源
+    }
+
+    virtual ~Lock()
+    {
+        servo.detach(); //释放舵机对象持有的资源
+    }
+
+    //开锁
+    void open()
+    {
+        servo.write(endDegree);
+    }
+
+    //关锁
+    void close()
+    {
+        servo.write(startDegree);
+    }
+
+    void write(int d){
+        servo.write(d);
+    }
+
+    //先开再关,即开门
+    void unlock()
+    {
+        open();
+        delay(2000);
+        close();
+    }
+};
